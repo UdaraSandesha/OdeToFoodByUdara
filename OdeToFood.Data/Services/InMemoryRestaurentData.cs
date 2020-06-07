631be434cc94bb91f8f7af372c82227919fc130b
UdaraSandesha <udarasandesha@gmail.com>
2020-06-07 23:07:07 +0530

    first commit

---
+++ OdeToFood.Data/Services/InMemoryRestaurentData.cs
@@ -0,0 +1,25 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurentData : IRestaurentData
+    {
+        List<Restaurent> restaurents;
+        public InMemoryRestaurentData()
+        {
+            restaurents = new List<Restaurent>()
+            {
+                new Restaurent{Id = 1, Name = "Sathira" ,Cuisine = CuisineType.Indian},
+                new Restaurent{Id = 2, Name = "Odara" ,Cuisine = CuisineType.French},
+                new Restaurent{Id = 3, Name = "Nirmala" ,Cuisine = CuisineType.Italian}
+            };
+        }
+        public IEnumerable<Restaurent> GetAll()
+        {
+            return restaurents.OrderBy(r => r.Name);
+        }
+    }
+}
